09250da02ee8a96e06ef4f586e359911f8f83c9c
Delia Suciu <delia.suciu@tecknoworks.com>
2017-07-18 12:37:01 +0300

    dresses

---
+++ BusinessLayer/DressManager.cs
@@ -0,0 +1,29 @@
+﻿using Abstracts.IManagers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects.Dtos;
+using Abstracts;
+using BusinessObjects.Entities;
+using DataLayer;
+using BusinessObjects;
+
+namespace BusinessLayer
+{
+    public class DressManager : IDressManager
+    {
+        IRepository<Dress> _repository;
+
+        public DressManager() {
+            _repository = new Repository<Dress>();
+        }
+
+        public IEnumerable<DressDto> GetAll()
+        {
+            var result = _repository.GetAll().ToDressDto();
+            return result;
+        }
+    }
+}
---
+++ ForFashion/Controllers/DressController.cs
@@ -0,0 +1,32 @@
+﻿using Abstracts.IManagers;
+using BusinessObjects.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ForFashion.Controllers
+{
+    public class DressController : ApiController
+    {
+        private IDressManager _dressManager;
+
+        public DressController()
+        {
+            var cont = DIContainer.Instance;
+            _dressManager = cont.Resolve<IDressManager>();
+        }
+
+        /// <summary>
+        /// Returns a list of dresses
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<DressDto> Get()
+        {
+            var result = _dressManager.GetAll();
+            return result;
+        }
+    }
+}
