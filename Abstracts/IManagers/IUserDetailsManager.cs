3208014e853e61acb01e5533bfc902452dd06fc6
deliasuciu <s.delia4@yahoo.ro>
2017-07-18 10:30:19 +0300

    Add files via upload

---
+++ Abstracts/IManagers/IUserDetailsManager.cs
@@ -0,0 +1,15 @@
+﻿using BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracts
+{
+    public interface IUserDetailsManager
+    {
+        ApplicationUser GetApplicationUser(string id);
+        void AddUserDetails(string id);
+    }
+}
