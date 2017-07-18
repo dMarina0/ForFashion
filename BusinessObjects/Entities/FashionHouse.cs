083a0fe4a9566ea7874ddec66dc1203860c455c1
Roxana Apofaian <roxana.apofaian@tecknoworks.com>
2017-07-18 15:45:42 +0300

    create FashionHouse table

---
+++ BusinessObjects/Entities/FashionHouse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Entities
+{
+    public class FashionHouse
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
