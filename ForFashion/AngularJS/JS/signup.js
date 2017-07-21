3dc7ecc4879f631e55345c895f024abe2bc115e9
Doci Marina <doci.marina17@yahoo.com>
2017-07-21 16:45:36 +0300

    SignUp  Angular

---
+++ ForFashion/AngularJS/JS/signup.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+var myApp = angular.module('myApp', []);
+
+myApp.controller('signupController', ['$scope','$window','signupService',function($scope,$window,signupService){
+
+    $scope.registration = {
+        Email: "",
+        Password: "",
+        ConfirmPassword:""
+
+    }
+    $scope.signUp = function () {
+        signupService.saveRegistration($scope.registration).then(function () {
+            alert("Registered");
+        },function(){
+            alert("Failed");
+        
+        })
+    }
+
+
+
+}])
+
+myApp.factory('signupService', ['$http', function ($htpp) {
+
+    signupService = {};
+    signupService.SaveRegistration = function (registration) {
+        return $http.post('/api/account/register',registration)
+    }
+
+    return signupService;
+})
