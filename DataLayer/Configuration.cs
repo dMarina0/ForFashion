389cfc8e12ac0a686aa6308a575230d1e140af04
Delia Suciu <delia.suciu@tecknoworks.com>
2017-07-28 13:29:42 +0300

    seed collection

--- DataLayer/Configuration.cs
+++ DataLayer/Configuration.cs
@@ -77,10 +77,19 @@ namespace DataLayer
                new Dress() { Id = 19, Name = "Multicoloured stripped dress" },
                new Dress() { Id = 20, Name = "Denim mini dress" }
                );
+            var fh1 = new FashionHouse() { Id = 1, Name = "Versace", PhoneNumber = "+40(0)31 6300031" };
+            var fh2 = new FashionHouse() { Id = 2, Name = "Chanel", PhoneNumber = "+40(0)31 6355531" };
+            var fh3 = new FashionHouse() { Id = 3, Name = "Gucci", PhoneNumber = "+40(0)31 8975531" };
             context.FashionHouse.AddOrUpdate(
                 f => f.Id,
-                new FashionHouse() { Id = 1, Name = "Versace", PhoneNumber = "+40(0)31 6300031" },
-                new FashionHouse() { Id = 2, Name = "Chanel", PhoneNumber = "+40(0)31 6355531" });
+                fh1,
+                fh2,
+                fh3);
+            context.Collection.AddOrUpdate(
+                c => c.Id,
+                new Collection() { Id = 1, Name = "Spring-Summmer",year = 2016, FashionHouse = fh1 },
+                new Collection() { Id = 2, Name = "Autumn-Winter",year = 2017, FashionHouse = fh2 },
+                new Collection() { Id = 3, Name = "Spring-Summer", year = 2017, FashionHouse = fh3 });
            
             context.SaveChanges(); 
             base.Seed(context);
