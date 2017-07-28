c009b24daebf9f622145b2e81f9a815253802d1c
Delia Suciu <delia.suciu@tecknoworks.com>
2017-07-28 11:00:22 +0300

    carrousel

---
+++ ForFashion/Scripts/app/core/base.controller.ts
@@ -0,0 +1,37 @@
+﻿
+abstract class BaseController  {
+   
+    constructor() {
+    } 
+
+    protected loadScript(url: string, callback: Function = null): void {
+
+        //let script: any = (<HTMLScriptElement[]><any>document.createElement("script"));
+        //script.type = "text/javascript";
+
+        //if (script.ReadyState) {  //IE
+        //    script.onreadystatechange = function () {
+        //        if (script.readyState == "loaded" ||
+        //            script.readyState == "complete") {
+        //            script.onreadystatechange = null;
+        //            callback();
+        //        }
+        //    };
+        //} else {  //Others
+        //    script.onload = function () {
+        //        callback();
+        //    };
+        //}
+
+        var script = document.createElement('script');
+        script.src = url;
+
+        if (callback != null) {
+            script.onload = function () {
+                callback();
+            };
+        }
+
+        document.body.appendChild(script);
+    }
+}
---
+++ ForFashion/Scripts/app/register/register.controller.js
@@ -0,0 +1,13 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var RegisterController = (function (_super) {
+    __extends(RegisterController, _super);
+    function RegisterController() {
+        _super.call(this);
+    }
+    return RegisterController;
+}(BaseController));
+//# sourceMappingURL=register.controller.js.map
